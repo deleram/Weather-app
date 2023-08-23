af3f45675953709ce2f8f95dc85973cf80d58eb6
deleram <delaramfaetoot@gmail.com>
2023-08-23 15:31:20 +0330

    forecast is completed

--- app.js
+++ app.js
@@ -14,9 +14,57 @@ function dateUpdate(timestamp) {
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let Day = days[day];
+  // upcoming days
+  for (let i = 1; i < 6; i++) {
+    let tempday = document.querySelector(`.day${i}`);
+    tempday.innerHTML = days[(day + i) % 7];
+  }
   return [`${Day}, ${hour}:${minute}`, day];
 }
 
+function forecast(response) {
+  weather_icon = {
+    "01d": "fa-regular fa-sun",
+    "02d": "fa-solid fa-cloud-sun",
+    "03d": "fa-solid fa-cloud",
+    "04d": "fa-solid fa-cloud",
+    "09d": "fa-solid fa-cloud-rain",
+    "10d": "fa-solid fa-cloud-sun-rain",
+    "11d": "fa-solid fa-cloud-bolt",
+    "13d": "fa-regular fa-snowflake",
+    "50d": "fa-solid fa-smog",
+    "01n": "fa-regular fa-sun",
+    "02n": "fa-solid fa-cloud-sun",
+    "03n": "fa-solid fa-cloud",
+    "04n": "fa-solid fa-cloud",
+    "09n": "fa-solid fa-cloud-rain",
+    "10n": "fa-solid fa-cloud-sun-rain",
+    "11n": "fa-solid fa-cloud-bolt",
+    "13n": "fa-regular fa-snowflake",
+    "50n": "fa-solid fa-smog",
+  };
+  for (let i = 1; i < 6; i++) {
+    // max and min temp forecast
+    let maxt = response.data.daily[i - 1].temp.max;
+    let mint = response.data.daily[i - 1].temp.min;
+    let maxtemp = document.querySelector(`#day${i}max`);
+    let mintemp = document.querySelector(`#day${i}min`);
+    maxtemp.innerHTML = Math.round(maxt);
+    mintemp.innerHTML = Math.round(mint);
+    // icon forecast
+    let icon = response.data.daily[i - 1].weather[0].icon;
+    let Upcomingicon = document.querySelector(`#icon${i}`);
+    Upcomingicon.className = weather_icon[icon];
+    Upcomingicon.classList.add("icon");
+    Upcomingicon.classList.add(`icon${i}`);
+  }
+}
+
+function forecast_req(lon, lat) {
+  let apikey = "c8a77112b2faf6684bb4b21a0aa778ae";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+  axios.get(url).then(forecast);
+}
 function updateData(response) {
   //city name
   let city = document.querySelector("h1#city");
@@ -67,6 +115,9 @@ function updateData(response) {
 
   let main_icon = document.querySelector(".main_icon i");
   main_icon.className = weather_icon[icon];
+  let lon = response.data.coord.lon;
+  let lat = response.data.coord.lat;
+  forecast_req(lon, lat);
 }
 
 function Save_city(event) {
@@ -77,7 +128,7 @@ function Save_city(event) {
 }
 
 function cityEnter(enteredCity) {
-  let apiKey = "5c9688543e1981e4307feea878cc3a57";
+  let apiKey = "c8a77112b2faf6684bb4b21a0aa778ae";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${enteredCity}&appid=${apiKey}&units=metric`;
   axios.get(url).then(updateData);
 }
